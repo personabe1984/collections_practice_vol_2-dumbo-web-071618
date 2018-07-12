066e8e2632cc51fafb04aea7677551e9110308af
Hanbi Choi <personabe1984@gmail.com>
2018-07-12 19:45:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -71,5 +71,16 @@ def merge_data(keys, values)
 end
 
 def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
 end
 
