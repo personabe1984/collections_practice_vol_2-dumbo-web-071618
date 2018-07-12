9b4a6f36ce347deb888380d43fc42cbc4062838a
Hanbi Choi <personabe1984@gmail.com>
2018-07-12 19:45:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,7 +34,7 @@ end
 
 def find_cool(cool_data)
   found_cool_arr = []
-  array.each do |element|
+  cool_data.each do |element|
     if element[:temperature] == "cool"
       found_cool_arr << element 
     end
@@ -42,13 +42,32 @@ def find_cool(cool_data)
   found_cool_arr
 end
 
-def count_elements(hash_elements)
-  hash_elements.each do |array_element|
-    puts array_element
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end
+  return array.uniq
 end
 
-def merge_data(keys, data)
-  
+def merge_data(keys, values)
+  container = []
+  keys.each do |person_name|
+    name = person_name[:first_name]
+    values.each do |person_data|
+      if person_data[name]
+        merged_person = person_data[name]
+        merged_person[:first_name] = name
+        container << merged_person
+      end
+    end
+  end
+  #container
 end
 
 def organize_schools(schools)
